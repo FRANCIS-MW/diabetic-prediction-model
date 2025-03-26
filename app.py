613178852e9676acec627845a0b9f7af5930decf
FRANCIS-MW <129581891+FRANCIS-MW@users.noreply.github.com>
2025-03-26 20:58:34 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,15 +2,16 @@ import pickle
 import streamlit as st
 import pandas as pd
 
-#load model
+# Load model
 @st.cache_resource
 def load_model():
-  with open('model.pkl','rb') as file:
-    model= pickle.load(file)
-return load_model
+    with open('model.pkl', 'rb') as file:
+        model = pickle.load(file)
+    return model  # Fixed return statement
 
+model = load_model()  # Call the function
 
-st.title('Diabetis Predictor')
-st.write('This app help as to check whether a patient has diabetis')
+st.title('Diabetes Predictor')
+st.write('This app helps to check whether a patient has diabetes')
 
 st.sidebar.header('Input Features')
